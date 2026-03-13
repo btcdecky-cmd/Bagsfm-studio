68c10aec5e2dc419219e7fe2a855906ee8b014f3
btcdecky-cmd <btcdecky@gmail.com>
2026-03-13 12:25:43 +0100

    Create setup.sh file with initial setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Clone the repository
+git clone https://github.com/btcdecky-cmd/Bagsfm-studio.git
+cd Bagsfm-studio
+
+# Install dependencies and run the development server
+npm install
+npm run dev
